9a2f768e2735be01bf0ffeda5c6d0f852e732d44
DonnaIB <donnaimrie@gmail.com>
2020-12-16 13:27:47 +0000

    added markers

--- map.js
+++ map.js
@@ -1,12 +1,21 @@
 function initMap() {
-  const myLatLng = { lat: -25.363, lng: 131.044 };
+  const myLatLng = { lat: 53.31952608924311, lng: -6.2276343192623775 };   
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 12,
     center: myLatLng,
   });
-  new google.maps.Marker({
-    position: myLatLng,
-    map,
-    title: "Hello World!",
-  });
+
+
+const myMarks = [
+    {"lat": 53.298750804089785, "lng": -6.178747830803757, "name": "HQ"},
+    {"lat": 53.33821423236237, "lng": -6.237388486160337, "name": "Intercambio"},  
+];
+
+for(let i=0; i < myMarks.length; i++) {
+    const marker = new google.maps.Marker ({
+        position: new google.maps.LatLng(myMarks[i].lat, myMarks[i].lng),
+        map: map,
+        title: myMarks[i].name
+    })
+}
 }
