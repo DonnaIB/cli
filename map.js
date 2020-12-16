366f72c71d7d917449193460bdf98067c69647b7
DonnaIB <donnaimrie@gmail.com>
2020-12-16 14:01:39 +0000

    added info box and animation to google markers

--- map.js
+++ map.js
@@ -7,18 +7,40 @@ function initMap() {
 
 
 const myMarks = [
-    {"lat": 53.298750804089785, "lng": -6.178747830803757, "name": "HQ"},
-    {"lat": 53.33821423236237, "lng": -6.237388486160337, "name": "Intercambio"},  
+    {"lat": 53.298750804089785, "lng": -6.178747830803757, "name": "HQ", "info": "This is our HQ, where we plan all our amazing events and create quizes to help you learn!"},
+    {"lat": 53.33821423236237, "lng": -6.237388486160337, "name": "Intercambio", "info": "Here is where our intercambios take place every Monday and Friday"},  
 ];
 
+var InfoObj = [];
+
 for(let i=0; i < myMarks.length; i++) {
-    
+    let contentString = '<h3>' + myMarks[i].name + '</h3>' + '<p>' + myMarks[i].info + '</p>' + '<a href="https://developers.google.com/maps/documentation/javascript/overview">' + 'Click me!' + '</a>';
     const marker = new google.maps.Marker ({
         position: new google.maps.LatLng(myMarks[i].lat, myMarks[i].lng),
         map: map,
-        title: myMarks[i].name
+        title: myMarks[i].name,
+        animation: google.maps.Animation.DROP,
     });
-    console.log('working');
+
+    const infowindow = new google.maps.InfoWindow({
+        content: contentString,
+        maxWidth: 500
+    });
+
+    marker.addListener('click', function() {
+        closeOtherInfo();
+        infowindow.open(map, marker);
+        InfoObj[0] = infowindow;
+    })
+
+    function closeOtherInfo() {
+        if(InfoObj.length > 0) {
+            InfoObj[0].set("marker", null)
+            InfoObj[0].close();
+            InfoObj[0].length = 0;
+        }
+    }
+
 }
 }
 
